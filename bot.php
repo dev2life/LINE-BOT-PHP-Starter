8c571adb8091edb933b1ac823c34ebd4f7f6c155
Asimov <dev2life@gmail.com>
2016-10-17 17:57:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			{
 				$messages = [
 					'type' => 'text',
-					'text' => $text
+					'text' => $text.$replyToken
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
