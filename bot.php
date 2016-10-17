4ed5499b2a6b9830ea0301b7c6abe9a48fcf11dc
Asimov <dev2life@gmail.com>
2016-10-17 15:50:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,8 +31,8 @@ if (!is_null($events['events'])) {
 			else $text = '';
 			*/
 			
-			$text = file_get_contents('http://democlaimpa.rvp.co.th/Services/LineTest.ashx?text=' . $text);
-			$text = $text . '1234'
+			//$text = file_get_contents('http://democlaimpa.rvp.co.th/Services/LineTest.ashx?text=' . $text);
+			$text = $text
 			if($text != '')
 			{
 				$messages = [
